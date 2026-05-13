cc9b9f97f588b6c4f849036c66f5f133083adfc8
karamkallab <karamkallab693@gmail.com>
2026-05-13 20:47:11 +0200

    skapade test filer

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ const path = require('path');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+    mode: "development",   // 👈 LÄGG TILL DEN HÄR
+
     entry: './src/index.js',
+
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
+
     plugins: [
         new CopyWebpackPlugin({
             patterns: [{
